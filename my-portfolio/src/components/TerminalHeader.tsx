44c60bcf9674da76a2234ac8466acd7cafc1acb8
Inam Ul Haq <inam101001@gmail.com>
2024-12-02 19:28:57 +0500

    fixes for build

--- my-portfolio/src/components/TerminalHeader.tsx
+++ my-portfolio/src/components/TerminalHeader.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Terminal as TerminalIcon, Minus, X, Square } from 'lucide-react';
+import React from "react";
+import { Terminal as TerminalIcon } from "lucide-react";
 
 const TerminalHeader: React.FC = () => {
   return (
